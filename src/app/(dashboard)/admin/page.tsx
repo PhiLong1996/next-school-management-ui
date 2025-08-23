4e72afe0c4941f75eb51ba0971d89e1a3de90089
unknown <philongtran1996@gmail.com>
2025-08-23 23:55:35 +0700

    done ui for admin page

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,7 +1,46 @@
+import Announcements from "@/components/Announcements"
+import AttendanceChart from "@/components/AttendanceChart"
+import CountChart from "@/components/CountChart"
+import EventCalender from "@/components/EventCalender"
+import FinanceChart from "@/components/FinanceChart"
+import UserCard from "@/components/UserCard"
 
 const AdminPage = () => {
     return (
-        <div className="">AdminPage</div>
+        <div className="p-4 flex gap-4 flex-col md:flex-row">
+            {/*LEFT*/ }
+            <div className="w-full lg:w-2/3 flex flex-col gap-8">
+                {/*User Card*/ }
+                <div className="flex justify-between gap-4 flex-wrap">
+                    <UserCard type="student"/>
+                    <UserCard type="teacher"/>
+                    <UserCard type="parent"/>
+                    <UserCard type="staff"/>
+                </div>
+                {/* Middle Charts */}
+                <div className="flex gap-4 flex-col lg:flex-row">
+                    {/* Count Chart */}
+                    <div className="w-full lg:w-1/3 h-[450px]">
+                        <CountChart/>
+                    </div>
+                    {/* Attendance Chart */}
+                    <div className="w-full lg:w-2/3 h-[450px]">
+                        <AttendanceChart/>
+                    </div>
+                </div>
+                {/* Bottom Charts */}
+                <div className="w-full h-[500px]">
+                    <FinanceChart/>
+                </div>
+            </div>
+             
+
+            {/*Right*/ }
+            <div className="w-full lg:w-1/3 flex flex-col gap-8">
+                <EventCalender/>
+                <Announcements/>
+            </div>
+        </div>
     )
 }
 
